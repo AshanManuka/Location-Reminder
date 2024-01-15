a18fd0c5b4b63a84ec4a3db480fb7483aa3cbcc3
ashan_manuka <manukajayarathne.coma@gmail.com>
2024-01-15 22:11:55 +0530

    integrate google map view

--- LRApp/screen/HomeScreen.js
+++ LRApp/screen/HomeScreen.js
@@ -1,68 +1,81 @@
-import React from 'react';
-import { View, Pressable, StyleSheet, Text, TextInput } from "react-native";
+import React, { useState } from 'react';
+import { View, Pressable, StyleSheet, Text, TextInput } from 'react-native';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 
+const HomeScreen = ({ navigation }) => {
+  const [startLocation, setStartLocation] = useState('');
+  const [endLocation, setEndLocation] = useState('');
 
-const HomeScreen = ({navigation}) => {
+  const startTrip = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
 
-    const [startLocation, setStartLocation] = React.useState('');
-
-    const startTrip = () => {
-        alert("Start Trip");
+    if (status !== 'granted') {
+      alert('Location services are required to start the trip.');
+      return;
     }
+    alert('Start Trip');
 
-    const changeStartLocation = (inputText) => {
-        setStartLocation(inputText);
+    //  Location.watchPositionAsync({ accuracy: Location.Accuracy.High, timeInterval: 1000, distanceInterval: 1 }, (location) => {
+    //    console.log('Location:', location);
+    //  });
+  };
 
-    }
+  const changeStartLocation = (inputText) => {
+    setStartLocation(inputText);
+  };
 
+  const changeEndLocation = (inputText) => {
+    setEndLocation(inputText);
+  };
 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Hello Ashan,</Text>
 
-
-    return(
-        <View style={styles.container}>
-
-            <Text style={styles.mainText}>Hello Ashan,</Text>
-
-            <View style={styles.startBtnSec}>
-                <Pressable style={styles.startBtn}
-                    onPress={startTrip}
-                >
-                <Text style={styles.btnText}>Start a Trip</Text> 
-                </Pressable>
-            </View>
-
-
-            <View style={styles.subView}>
-
-                <TextInput style={styles.inputItemsOne}
-                onChangeText={changeStartLocation}
-                placeholder='Start Location'
-                value={startLocation}
-                />
-
-
-
-            </View>
-
-
-
-
-
-        <View style={styles.btnSec}>
-        <Pressable style={styles.button}
-        onPress={() => navigation.navigate('Game')}>
-        <Text style={styles.btnText}>Logs</Text>  
+      <View style={styles.startBtnSec}>
+        <Pressable style={styles.startBtn} onPress={startTrip}>
+          <Text style={styles.btnText}>Start a Trip</Text>
         </Pressable>
-        </View>
+      </View>
+
+      <View style={styles.subView}>
+        <TextInput
+          style={styles.inputItemsOne}
+          onChangeText={changeStartLocation}
+          placeholder="Start Location"
+          value={startLocation}
+        />
+
+        <TextInput
+          style={styles.inputItemsTwo}
+          onChangeText={changeEndLocation}
+          placeholder="Final Location"
+          value={endLocation}
+        />
+
+
+        <MapView style={styles.subMapView}>
+
+        </MapView>
 
 
 
+      </View>
 
+      <View style={styles.btnSec}>
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate('Game')}
+        >
+          <Text style={styles.btnText}>Logs</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
-        </View>
-    );
-}
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
@@ -76,6 +89,7 @@ const styles = StyleSheet.create({
         fontSize:20,
         width:'80%',
         fontWeight:'bold',
+        marginTop:'12%'
     },
     startBtnSec:{
       marginTop:'10%',
@@ -103,7 +117,8 @@ const styles = StyleSheet.create({
         backgroundColor:'#0a3d62',
         marginTop:'10%',
         width:'95%',
-        height:'60%'
+        height:'60%',
+        borderRadius:10
     },
     inputItemsOne:{
         height: 40,
@@ -118,8 +133,27 @@ const styles = StyleSheet.create({
         color:'#fff',
         fontSize:18,
     },
+    inputItemsTwo:{
+        height: 40,
+        width:'40%',
+        borderWidth: 1,
+        marginTop:'-10%',
+        marginLeft:'55%',
+        borderTopColor:'#0a3d62',
+        borderLeftColor:'#0a3d62',
+        borderRightColor:'#0a3d62',
+        borderBottomColor:'#fff',
+        color:'#fff',
+        fontSize:18,
+    },
     btnSec:{
         marginTop:'20%'
-    }
+    },
+    subMapView:{
+      width:'95%',
+      height:'80%',
+      marginTop:'10%',
+      marginLeft:'2.5%',
+    },
   });
   
